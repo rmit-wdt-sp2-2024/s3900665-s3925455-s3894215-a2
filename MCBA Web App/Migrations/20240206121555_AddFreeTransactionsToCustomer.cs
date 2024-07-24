b0433bec54c8acbf044d599b39e43c9d7ca632a4
s3925455 <90074276+s3925455@users.noreply.github.com>
2024-07-24 18:30:37 +1000

    added migrations

---
+++ MCBA Web App/Migrations/20240206121555_AddFreeTransactionsToCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MCBA_Web_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFreeTransactionsToCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FreeTransactions",
+                table: "Customer",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FreeTransactions",
+                table: "Customer");
+        }
+    }
+}
---
+++ MCBA Web App/Migrations/20240209113255_PaymentsStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MCBA_Web_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentsStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "BillPay",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "BillPay");
+        }
+    }
+}
