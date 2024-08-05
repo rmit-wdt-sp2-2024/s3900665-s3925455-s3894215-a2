591a049d1d45097e73f09ecb35b9335af06f31e0
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-05 21:29:47 +1000

    Revert "Added limits and regEx to Customer.cs"
    This reverts commit 0578ccc81edea12f17c6df62498ea4dc6f9bd88a.

--- MCBA Web App/Models/Customer.cs
+++ MCBA Web App/Models/Customer.cs
@@ -7,30 +7,18 @@ namespace MCBA_Web_App.Models
         [Key]
         public required int CustomerID { get; set; }
 
-        [StringLength(50)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public required string Name { get; set; }
 
-        [Range(100000000, 999999999, ErrorMessage = "TFN must be a 9-digit number")]
         public int? TFN { get; set; }
 
-        [StringLength(50)]
-        [RegularExpression(@"^[A-Za-z0-9]+[A-Za-z0-9""'\s-]*$")]
         public string? Address { get; set; }
 
-        [StringLength(40)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string? City { get; set; }
 
-        [StringLength(100)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string? State { get; set; }
 
-        [Range(0001, 9999, ErrorMessage = "The postcode must be a 4-digit number")]
         public int? Postcode { get; set; }
 
-        [StringLength(12)]
-        [RegularExpression(@"^(\+61|0)[ -]?[2-478][0-9]{1,4}[ -]?[0-9]{3,4}[ -]?[0-9]{3,4}$", ErrorMessage = "Invalid phone number format.")]
         public string? Mobile { get; set; }
 
         public virtual List<Account> Accounts { get; set; }
