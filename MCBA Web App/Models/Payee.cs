e5fe1467ec6c059a391d6e66b38020f0c218423d
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-05 21:11:20 +1000

    Added full methods to Payee.cs

--- MCBA Web App/Models/Payee.cs
+++ MCBA Web App/Models/Payee.cs
@@ -1,6 +1,29 @@
-﻿namespace MCBA_Web_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCBA_Web_App.Models
 {
     public class Payee
     {
+        [Key]
+        public int PayeeID { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(50), RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Address { get; set; }
+
+        [Required, StringLength(40), RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string City { get; set; }
+
+        [Required, StringLength(3, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string State { get; set; }
+
+        [Required, Range(1000, 9999)]
+        public int Postcode { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
     }
 }
