d82eea085b234e901599557ba370abce38339ad7
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-05 21:08:41 +1000

    Added getter setter methods

--- MCBA Web App/Models/Customer.cs
+++ MCBA Web App/Models/Customer.cs
@@ -1,6 +1,27 @@
-﻿namespace MCBA_Web_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCBA_Web_App.Models
 {
     public class Customer
     {
+        [Key]
+        public required int CustomerID { get; set; }
+
+        public required string Name { get; set; }
+
+        public int? TFN { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public int? Postcode { get; set; }
+
+        public string? Mobile { get; set; }
+
+        public virtual List<Account> Accounts { get; set; }
+        public Login Login { get; set; }
     }
 }
