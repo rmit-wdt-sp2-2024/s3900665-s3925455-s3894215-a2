1e837e1f63bb107ed9c2313e80132894b7c2d71b
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-05 21:44:08 +1000

    Added MAIN homepade view

--- MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
+++ MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07cc263b531bbb1f436d2b95fbbed3f1631b6e1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db69ba2978149c207c7513e2db2ec76eb162704")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
