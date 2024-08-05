bc8c7073fe85ab2ff19ab62aae63f476b8e10264
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-05 21:29:47 +1000

    Revert "Added all getter/setter methods for BillPay.cs"
    This reverts commit c3a0bbc20794f1d16900c274f873e243fc17d31e.

--- MCBA Web App/Models/BillPay.cs
+++ MCBA Web App/Models/BillPay.cs
@@ -1,34 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace MCBA_Web_App.Models
+﻿namespace MCBA_Web_App.Models
 {
-    public enum PaymentStatus
-    {
-        Pending,
-        Success,
-        Failed
-    }
     public class BillPay
     {
-        [Key]
-        public required int BillPayID { get; set; }
-
-        public required int AccountNumber { get; set; }
-
-        public required int PayeeID { get; set; }
-
-        [Column(TypeName = "decimal(18, 2)"), DataType(DataType.Currency)]
-        public required decimal Amount { get; set; }
-
-        [Required]
-        public DateTime ScheduleTimeUtc { get; set; }
-
-        public required char Period { get; set; }
-
-        public PaymentStatus Status { get; set; }
-
-        [ForeignKey("AccountNumber")]
-        public virtual Account Account { get; set; }
     }
 }
