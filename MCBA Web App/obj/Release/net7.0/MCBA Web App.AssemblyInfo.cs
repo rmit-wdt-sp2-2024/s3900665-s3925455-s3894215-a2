10dec66f14edd5f6eecfcbc1f5648a7793d6cb12
idrissbankahulu <idriss@live.com.au>
2024-08-07 18:46:01 +1000

    Added empty Index and Deposit classes

--- MCBA Web App/obj/Release/net7.0/MCBA Web App.AssemblyInfo.cs
+++ MCBA Web App/obj/Release/net7.0/MCBA Web App.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c13fba2b4257ffcd5d2961bd789e7161358a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App")]
