bec4ef403855f592a4b3a7f42c4105e1cd3221a2
idrissbankahulu <idriss@live.com.au>
2024-08-07 18:47:16 +1000

    added empty Withdrawl class

--- MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
+++ MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c13fba2b4257ffcd5d2961bd789e7161358a51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a680e4a8aa984502b3b0c27ed7ea817fb6f9589c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
