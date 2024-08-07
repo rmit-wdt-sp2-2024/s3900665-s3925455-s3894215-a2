39d841cf1ac2175818d596d5be0ba2cc3857cb3d
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-07 20:53:53 +1000

    Added EVM model

--- MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
+++ MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be7814263ca82cc48036c808743d05c9796b6ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab90fcbe20063549b4b8bcfd0cea85ab5e48181")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
