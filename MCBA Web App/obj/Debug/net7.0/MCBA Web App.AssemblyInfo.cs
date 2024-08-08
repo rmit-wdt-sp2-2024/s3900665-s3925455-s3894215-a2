74e98ef7c3c2c3b0a641825a25ddc621a2d40585
idrissbankahulu <idriss@live.com.au>
2024-08-08 18:10:56 +1000

    Added old password field

--- MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
+++ MCBA Web App/obj/Debug/net7.0/MCBA Web App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8ae6611ed9191e6ed15f0449b259b93be0cb3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe0af03d777c3d1ff0225feba5a7a2fd8a36e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MCBA Web App/obj/Release/net7.0/MCBA Web App.AssemblyInfo.cs
+++ MCBA Web App/obj/Release/net7.0/MCBA Web App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe0af03d777c3d1ff0225feba5a7a2fd8a36e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCBA Web App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
