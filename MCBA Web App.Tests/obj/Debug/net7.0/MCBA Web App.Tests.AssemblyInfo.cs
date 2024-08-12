749b74b9e3632a067f35a37ad0d447ba8fe2a896
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-08-12 21:00:13 +1000

    Removed extra header and moved footer to absolute bottom

--- MCBA Web App.Tests/obj/Debug/net7.0/MCBA Web App.Tests.AssemblyInfo.cs
+++ MCBA Web App.Tests/obj/Debug/net7.0/MCBA Web App.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCBA Web App.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a504e7c3ebaf2f88c82c7db3a6664b1fa30f23a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a067c827aa8eab78aaeb3bc2033e4fcff63b130f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCBA Web App.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCBA Web App.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
